5e96d5a158bf1a0206646c0aba3ae635fbd2286e
Harissh0110 <Harissh0110@gmail.com>
2024-05-23 19:49:30 +0530

    payment removal

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import LoginScreen from './screens/loginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
-import PaymentScreen from './screens/PaymentScreen';
+// import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen  from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
@@ -29,7 +29,7 @@ const App = () => (
           <Route path='/register'  element={<RegisterScreen />} />
           <Route path='/profile'  element={<ProfileScreen />} />
           <Route path='/shipping'  element={<ShippingScreen />} />
-          <Route path='/payment'  element={<PaymentScreen />} />
+          {/* <Route path='/payment'  element={<PaymentScreen />} /> */}
           <Route path='/placeorder'  element={<PlaceOrderScreen />} />
           <Route path='/order/:id'  element={<OrderScreen />} />
           <Route path='/product/:id' element={<ProductScreen />} />
